24ae2872137bb3b235f95f446cd2f42af0657231
Lisa Rozhkova <liza.roz@yandex.ru>
2024-02-06 12:10:20 +0300

    Refactor: move Point to utils + typing

--- src/algorithms/annealing_algorithm.py
+++ src/algorithms/annealing_algorithm.py
@@ -5,83 +5,11 @@ import numpy as np
 
 from utils.graph import Graph
 from utils.algorithm import Algorithm
+from utils.point import Point
 
 # Коэффициенты для расчета температуры в методе отжига
 INITIAL_TEMPERATURE = 100
 COOLING_RATE = 0.0003
-
-
-class Point:
-    '''
-    https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
-    '''
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    @staticmethod
-    def change_position(value):
-        x = random.random() * value
-        y = random.random() * value
-        return Point(x, y)
-
-    # Given three collinear points p, q, r, the function checks if
-    # point q lies on line segment 'pr'
-    @staticmethod
-    def onSegment(p, q, r):
-        if ((q.x <= max(p.x, r.x)) and (q.x >= min(p.x, r.x)) and (q.y <= max(p.y, r.y)) and (q.y >= min(p.y, r.y))):
-            return True
-        return False
-
-    @staticmethod
-    def orientation(p, q, r):
-        # to find the orientation of an ordered triplet (p,q,r)
-        # function returns the following values:
-        # 0 : Collinear points
-        # 1 : Clockwise points
-        # 2 : Counterclockwise
-
-        val = (float(q.y - p.y) * (r.x - q.x)) - (float(q.x - p.x) * (r.y - q.y)) 
-        if val > 0:
-            return 1    # Clockwise orientation
-        if val < 0:
-            return 2    # Counterclockwise orientation
-        return 0        # Collinear orientation
-
-    @staticmethod
-    def doIntersect(p1, q1, p2, q2):
-
-        # Find the 4 orientations required for
-        # the general and special cases
-        o1 = Point.orientation(p1, q1, p2)
-        o2 = Point.orientation(p1, q1, q2)
-        o3 = Point.orientation(p2, q2, p1)
-        o4 = Point.orientation(p2, q2, q1)
-
-        # General case
-        if ((o1 != o2) and (o3 != o4)):
-            return True
-
-        # Special Cases
-
-        # p1 , q1 and p2 are collinear and p2 lies on segment p1q1
-        if ((o1 == 0) and Point.onSegment(p1, p2, q1)):
-            return True
-
-        # p1 , q1 and q2 are collinear and q2 lies on segment p1q1
-        if ((o2 == 0) and Point.onSegment(p1, q2, q1)):
-            return True
-
-        # p2 , q2 and p1 are collinear and p1 lies on segment p2q2
-        if ((o3 == 0) and Point.onSegment(p2, p1, q2)):
-            return True
-
-        # p2 , q2 and q1 are collinear and q1 lies on segment p2q2
-        if ((o4 == 0) and Point.onSegment(p2, q1, q2)):
-            return True
-
-        # If none of the cases
-        return False
 
 
 class AnnealingAlgorithm(Algorithm):
@@ -147,7 +75,7 @@ class AnnealingAlgorithm(Algorithm):
             temp1 = self.pos[i]
             temp2 = self.pos[j]
             old_crossings = self.count_crossings()
-            self.pos[i], self.pos[j] = Point.change_position(1), Point.change_position(1)
+            self.pos[i], self.pos[j] = self.pos[i].change_position(1), self.pos[j].change_position(1)
             new_crossings = self.count_crossings()
             if new_crossings < old_crossings or math.exp((old_crossings-new_crossings)/temperature) > random.random():
                 continue
---
+++ utils/point.py
@@ -0,0 +1,107 @@
+import random
+
+
+class Point:
+    '''
+    Класс Point предназначен для хранения координат вершины на плоскости.
+
+    Реализация проверки пересечения двух отрезков взята с сайта:
+    https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
+    '''
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def change_position(self, value) -> 'Point':
+        '''
+        Меняет позицию точки на плоскости.
+
+        Args:
+        value (int/float): Коэффициент для изменения координат.
+
+        Returns:
+        Point: Точка с новыми координатами.
+        '''
+        self.x = random.random() * value
+        self.y = random.random() * value
+        return self.__class__(self.x, self.y)
+
+    @staticmethod
+    def onSegment(p: 'Point', q: 'Point', r: 'Point') -> bool:
+        '''
+        Даны 3 коллинеарные точки p, q, r.
+        Функция проверяет, лежит ли точка q на отрезке pr.
+
+        Args:
+        p (Point): Начало отрезка.
+        q (Point): Точка, которую надо проверить.
+        r (Point): Конец отрезка.
+
+        Returns:
+        bool: True если точка q лежит на отрезке pr, иначе False.
+        '''
+        if ((q.x <= max(p.x, r.x)) and (q.x >= min(p.x, r.x)) and (q.y <= max(p.y, r.y)) and (q.y >= min(p.y, r.y))):
+            return True
+        return False
+
+    @staticmethod
+    def orientation(p, q, r) -> int:
+        '''
+        Ориентация упорядоченного триплета (p, q, r).
+
+        Returns:
+        0: Collinear points
+        1: Clockwise points
+        2: Counterclockwise
+        '''
+        val = (float(q.y - p.y) * (r.x - q.x)) - (float(q.x - p.x) * (r.y - q.y)) 
+        if val > 0:
+            return 1    # Clockwise orientation
+        if val < 0:
+            return 2    # Counterclockwise orientation
+        return 0        # Collinear orientation
+
+    @staticmethod
+    def doIntersect(p1, q1, p2, q2) -> bool:
+        '''
+        Определяет, пересекаются ли два отрезка p1q1 и p2q2.
+
+        Args:
+        p1 (Point): Начало первого отрезка.
+        q1 (Point): Конец первого отрезка.
+        p2 (Point): Начало второго отрезка.
+        q2 (Point): Конец второго отрезка.
+
+        Returns:
+        bool: True если отрезки пересекаются, иначе False.
+        '''
+        # Находим 4 ориентации, необходимые для
+        # Общего и особенных случаев
+        o1 = Point.orientation(p1, q1, p2)
+        o2 = Point.orientation(p1, q1, q2)
+        o3 = Point.orientation(p2, q2, p1)
+        o4 = Point.orientation(p2, q2, q1)
+
+        # Общий случай
+        if ((o1 != o2) and (o3 != o4)):
+            return True
+
+        # Особенные случаи
+
+        # p1, q1 и p2 коллинеарные и p2 лежит на отрезке p1q1
+        if ((o1 == 0) and Point.onSegment(p1, p2, q1)):
+            return True
+
+        # p1, q1 и q2 коллинеарные и q2 лежит на отрезке p1q1
+        if ((o2 == 0) and Point.onSegment(p1, q2, q1)):
+            return True
+
+        # p2, q2 и p1 коллинеарные и p1 лежит на отрезке p2q2
+        if ((o3 == 0) and Point.onSegment(p2, p1, q2)):
+            return True
+
+        # p2, q2 и q1 коллинеарные и q1 лежит на отрезке p2q2
+        if ((o4 == 0) and Point.onSegment(p2, q1, q2)):
+            return True
+
+        return False
