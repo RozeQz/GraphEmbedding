7d68a9c979651951492df8e928a2ceef91389638
Lisa Rozhkova <liza.roz@yandex.ru>
2024-02-16 16:02:10 +0300

    Add handler for stopping testing

--- main.py
+++ main.py
@@ -32,18 +32,7 @@ def main() -> None:
         test = tm.create_test(tasks=tasks, time=20)
 
         test.start()
-        for task in test:
-            print(task.question)
-            for answer in task.options:
-                print(answer, end='\t')
-            print()
-            answer = input()
-            if task.check_answer(answer):
-                print(colored('Верно!', 'green'))
-            else:
-                print(colored('Неврно!', 'red'))
-        if test.get_state():
-            test.stop()
+
     else:
         try:
             with open("data/input.txt", "r", encoding="utf-8") as file:
@@ -81,7 +70,8 @@ def main() -> None:
                 new_graph = Graph(graph)
                 new_graph = nx.Graph(np.array(new_graph.matrix), nodetype=int)
 
-                nx.draw(new_graph, pos=planar, with_labels=True)
+                nx.draw(new_graph, pos=planar, with_labels=True,
+                        node_color="#0C8CE9")
                 plt.show()
 
         except FileNotFoundError as e:
--- src/education/testing.py
+++ src/education/testing.py
@@ -1,10 +1,40 @@
 from typing import List
 import threading
+import sys
+from termcolor import colored   # Убрать потом
 
 from src.education.task import Task
 
 
+class StopTestingException(Exception):
+    '''
+    Вызывается, когда тестирование завершается.
+    '''
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return f'StopTestingException: {self.message}'
+        return 'StopTestingException has been raised'
+
+
 class Testing():
+    '''
+    Класс для тестирования знаний пользователя.
+
+    Attributes:
+        tasks (List[Task]): Список заданий, включенных в тестирование.
+        timer (threading.Timer): Таймер для остановки тестирования.
+        points (int): Количество полученных баллов за тестирование.
+        current_task_index (int): Используется при итерировании по заданиям.
+
+    Raises:
+        StopTestingException: Когда тестирование завершается.
+    '''
     def __init__(self, tasks: List[Task], time: float = 3600):
         '''
         Инициализирует объект класса Testing, предназначенный для тестирования
@@ -13,10 +43,10 @@ class Testing():
         Args:
             tasks (List[Task]): Список заданий, включенных в тестирование.
             time (float): Время тестирования в секундах. По умолчанию тестирование длится час.
-            current_task_index (int): Используется при итерировании по заданиям.
         '''
         self.tasks = tasks
         self.timer = threading.Timer(time, self.stop)
+        self.points = 0
         self.current_task_index = None
 
     def __iter__(self):
@@ -37,12 +67,41 @@ class Testing():
         self.timer.start()
         print('Тестирование началось')
 
-    def stop(self) -> None:
+        for task in self:
+            print(task.question)
+            for answer in task.options:
+                print(answer, end='\t')
+            print()
+            answer = input()
+            if task.check_answer(answer):
+                print(colored('Верно!', 'green'))
+                self.points += 1
+            else:
+                print(colored('Неверно!', 'red'))
+            print()
+        if self.get_state():
+            self.stop(timeout=False)
+
+    def stop(self, timeout: bool = True) -> None:
         '''
         Остановка тестирования.
+
+        Args:
+            timeout (bool): Вышло ли время тестирования.
         '''
-        self.timer.cancel()
-        print('Тестирование завершено')
+        try:
+            if not timeout:
+                self.timer.cancel()
+                message = "Вы выполнили все задания. Тестирование завершено!"
+            else:
+                message = "Время вышло! Тестирование завершено!"
+            raise StopTestingException(message)
+        except StopTestingException as e:
+            self.__stop_handler(e)
+
+    def __stop_handler(self, exception):
+        print(exception.message)
+        print(f'Ваш результат: {self.points}/{len(self.tasks)}')
 
     def get_state(self) -> bool:
         '''
